d6d727b1f786eaa90544f7c4ab0916550999af89
DmitriyBartosh <Bartosh92@mail.ru>
2021-02-13 11:49:50 +0700

    12.02.21

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 // import Swiper JS
 import Swiper from 'swiper';
+import gsap from "gsap";
 
 const swiper = new Swiper('.categoryNav__container', {
   spaceBetween: 15,
@@ -16,3 +17,33 @@ const swiper = new Swiper('.categoryNav__container', {
     }
   }
 });
+
+// Анимация для списка ссылок и галереи (Freelance)
+let master = document.querySelectorAll(".master");
+
+function openGallery(i){
+  let gallary = document.querySelector("#gallary_" + [i]);
+  let gallaryPhoto = document.querySelector("#gallary_" + [i]).childNodes;
+  const openGallery = gsap
+    .timeline()
+    .set(gallary, {height: 508});
+}
+
+function openCallback(i){
+  let callback = document.querySelector("#callback_" + [i]);
+  let callbackLink = document.querySelector("#callback_" + [i]).childNodes;
+  const openCallback = gsap
+    .timeline()
+    .to(callback, {height: 188})
+    .from(callbackLink, {opacity: 0, y: 5, stagger: 0.1}, '-=0.2');
+
+}
+
+for (var i = 0; i < master.length; i++) {
+let linkGallary = document.querySelector("#linkGallery_" + [i+1]);
+let linkCallback = document.querySelector("#linkCallback_" + [i+1]);
+let index = i + 1;
+
+linkGallary.addEventListener("click", () => openGallery(index))
+linkCallback.addEventListener("click", () => openCallback(index))
+}
